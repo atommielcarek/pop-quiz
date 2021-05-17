cd110bcf821a56bce34b8d698f62fcbd1bf69d93
Prometheus <atommielcarek@gmail.com>
2021-05-16 22:59:45 -0600

    Added all .js , .html , and .css files for finished quiz game.

---
+++ questions.js
@@ -0,0 +1,29 @@
+var questions = [
+    {
+        title: "Which HTML element do we put the JavaScript inside?",
+        choices: ["<javascript>", "<js>", "<script>", "<div>" ],
+        answer: "<script>"
+    },
+    {
+        title: "Where is the correct placement to link a JavaScript file?",
+        choices: ["the <head> section", "the <body> section", "after the <footer> tag"],
+        answer: "the <body> section"
+    },
+    {
+        title: "what CLI is used to write 'Hello World' in an alert box?",
+        choices: ["alertBox('Hello World');", "msgBox('Hello World');", "alert('Hello World');", "alert(Hello World)" ],
+        answer: "alert('Hello World');"
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+        
+    },
+    {
+        title: "What html tag is NOT included in the HEAD tag?",
+        choices: ["link", "meta", "title", "header"],
+        answer: "header"
+    }
+
+]
